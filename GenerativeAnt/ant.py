344efdde2c4ea4466556ed97401d54ea469343a3
Evgeniy Sinyutin <dark_elf4@mail.ru>
2023-06-02 18:38:22 +0300

    added ant function

--- GenerativeAnt/ant.py
+++ GenerativeAnt/ant.py
@@ -3,36 +3,42 @@
 import py5
 
 class Ant:
-    def __init__(self, param1, param2, param3, param4, param5):
-        self.param1 = param1
-        self.param2 = param2
-        self.param3 = param3
-        self.param4 = param5
-        self.param5 = param5
+    def __init__(self, head_param, body_param, abdomen_param, legs_len_param):
+        self.head_param = head_param
+        self.body_param = body_param
+        self.abdomen_param = abdomen_param
+        self.legs_len_param = legs_len_param
         #Размеры головы
-        self.head_w = self.param1 * py5.random(30, 50) 
-        self.head_h = self.param1 * py5.random(40, 50)
+        self.head_h = py5.remap(head_param, 0, 1, 10, 40)
+        self.head_w = py5.remap(head_param, 0, 1, 20, 30)
+        #self.head_w = self.param1 * py5.random(30, 50) 
+        #self.head_h = self.param1 * py5.random(40, 50)
         #Размеры тела
-        self.body = (self.head_w + self.head_h) * self.param2
+        self.body = py5.remap(body_param, 0, 1, (self.head_w + self.head_h) / 4, (self.head_w + self.head_h) / 2) 
+        #self.body = (self.head_w + self.head_h) * self.param2
         #Размеры брюшка
-        self.abdomen_w = (self.body + self.head_h) / self.param3
-        self.abdomen_h = (self.abdomen_w * 4) / self.param3
+        self.abdomen_w = py5.remap(self.abdomen_param, 0, 1, (self.body + self.head_h)/3, (self.body + self.head_h))
+        #self.abdomen_w = (self.body + self.head_h) / self.param3
+        self.abdomen_h = py5.remap(self.abdomen_param, 0, 1, self.abdomen_w * 1.5, self.abdomen_w * 2)
         #Координаты начала лап
-        self.legs1_y = 5 + py5.random(0, 5 + self.param4) 
-        self.legs2_y = 10 + py5.random(0, 5 + self.param4) 
-        self.legs3_y = 15 + py5.random(0, 5 + self.param4) 
+        self.legs1_y = 5
+        self.legs2_y = 10
+        self.legs3_y = 15
         #Длина лап
-        self.legs1_l = py5.random(30, 30 + self.param5) 
-        self.legs2_l = py5.random(40, 40 + self.param5) 
-        self.legs3_l = py5.random(20, 20 + self.param5) 
+        self.legs1_l = py5.remap(self.legs_len_param, 0, 1, 15, 20)
+        self.legs2_l = py5.remap(self.legs_len_param, 0, 1, 25, 40)
+        self.legs3_l = py5.remap(self.legs_len_param, 0, 1, 30, 45)
         #Число изломов в лапах
-        self.legs1_n = 4
-        self.legs2_n = 3
-        self.legs3_n = 3
+        self.legs1_n = int(py5.remap(self.legs1_l, 15, 20, 2, 5))
+        self.legs2_n = int(py5.remap(self.legs2_l, 25, 40, 1, 3))
+        self.legs3_n = int(py5.remap(self.legs3_l, 30, 45, 2, 5))
         #Угол излома лап
-        self.legs1_angle = -40
-        self.legs2_angle = -30
-        self.legs3_angle = -10
+        self.legs1_angle = -(py5.remap(self.legs1_l, 15, 20, 30, 50))
+        self.legs2_angle = -(py5.remap(self.legs2_l, 25, 40, 30, 40))
+        self.legs3_angle = -(py5.remap(self.legs3_l, 30, 45, 10, 30))
+        #self.legs1_angle = -30 + py5.random_int(-20, -10)
+        #self.legs2_angle = -30 + py5.random_int(-10, -5)
+        #self.legs3_angle = -10 + py5.random_int(-10, -5)
          
     def show(self, x, y):
         py5.push_matrix()
@@ -43,8 +49,8 @@ class Ant:
         py5.rect_mode(py5.CENTER)
         #Отрисовка головы
         py5.rect(0, 0, self.head_w, self.head_h,
-                 self.param1 * (self.head_h - self.body), 
-                 self.param1 * (self.head_h - self.body),
+                 self.head_param * abs(self.head_h - self.body), 
+                 self.head_param * abs(self.head_h - self.body),
                  20,
                  20)
         #Отрисовка тела
@@ -53,11 +59,11 @@ class Ant:
         py5.ellipse(0, self.head_h/2 + self.body + self.abdomen_h/2, self.abdomen_w, self.abdomen_h)
         #Отрисовка лап
         leg_recursion(0, self.head_h + self.legs1_y, self.legs1_l, self.legs1_n, self.legs1_angle)
-        #left_leg_recursion(0, self.head_h + self.legs1_y, self.legs1_l, self.legs1_n, 180 - self.legs1_angle)
+        left_leg_recursion(0, self.head_h + self.legs1_y, self.legs1_l, self.legs1_n, -self.legs1_angle)
         leg_recursion(0, self.head_h + self.legs2_y, self.legs2_l, self.legs2_n, self.legs2_angle)
-        #leg_recursion(0, self.head_h + self.legs2_y, self.legs2_l, self.legs2_n, 180 - self.legs2_angle)
+        left_leg_recursion(0, self.head_h + self.legs2_y, self.legs2_l, self.legs2_n, - self.legs2_angle)
         leg_recursion(0, self.head_h + self.legs3_y, self.legs3_l, self.legs3_n, self.legs3_angle)
-        #leg_recursion(0, self.head_h + self.legs2_y, self.legs2_l, self.legs2_n, 180 - self.legs2_angle)
+        left_leg_recursion(0, self.head_h + self.legs3_y, self.legs3_l, self.legs3_n, - self.legs3_angle)
         
         
         py5.pop_matrix()
@@ -81,17 +87,17 @@ def leg_recursion(x, y, length, fracture_num, angle):
     leg_recursion(length, 0, length/2, fracture_num - 1, angle)
     py5.pop_matrix()
 
-"""
+
 def left_leg_recursion(x, y, length, fracture_num, angle):
     if fracture_num == 0:
         return
     py5.push_matrix()
     py5.translate(x, y)
     py5.rotate(py5.radians(angle))
-    py5.line(0, 0, length, 0)
-    left_leg_recursion(length, 0, length/2, fracture_num - 1, -180 + angle)
+    py5.line(0, 0, -length, 0)
+    left_leg_recursion(-length, 0, length/2, fracture_num - 1, angle)
     py5.pop_matrix()
-"""
+
         
     
     
--- GenerativeAnt/main.py
+++ GenerativeAnt/main.py
@@ -9,19 +9,76 @@ import py5
 from ant import Ant
 
 def setup():
-    global test_ant, ants
-    py5.size(900, 500)
+    global test_ant, ants, ants2, ants3, ants4
+    py5.size(1100, 700)
     py5.background(255)
+    #test_ant = Ant(py5.random(0.1, 0.8), py5.random(0.8, 0.9), 2.1, py5.random(5,7), 20)
     ants = []
+    ants2 = []
+    ants3 = []
+    ants4 = []
+    head_param = 0.0
+    body_param = 0.0
+    abdomen_param = 0.0
+    legs_len_param = 0.0
+    
+    for i in range(100):
+        ants.append(Ant(py5.random(0,1), py5.random(0, 1), py5.random(0, 1), py5.random(0, 1)))
+    """
     for i in range(10):
-        ants.append(Ant(py5.random(0.7, 1.1), py5.random(0.6, 0.9), 2.1, py5.random(5,7), 20))
-    
-    
+        ants2.append(Ant(0.5, body_param, 0.5, 0.5))
+        body_param += 0.1
+    for i in range(10):
+        ants3.append(Ant(0.5, 0.5, abdomen_param, 0.5))
+        abdomen_param += 0.1
+    for i in range(10):
+        ants4.append(Ant(0.5, 0.5, 0.5, legs_len_param))
+        legs_len_param += 0.1
+    """
     
 def draw():
-    global test_ant, ants
-    for i in range(len(ants)):
-        ants[i].show(90 * i, 250)
-    #test_ant2.show(100, 100)
-
+    global test_ant, ants, ants2, ants3, ants4
+    index1 = py5.random_int(0, len(ants) - 1)
+    index2 = py5.random_int(0, len(ants) - 1)
+    ant1 = ants[index1]
+    ant2 = ants[index2]
+    ant1.show(100, 500)
+    ant2.show(200, 500)
+    head_gene = [ant1.head_param, ant2.head_param]
+    body_gene = [ant1.body_param, ant2.body_param]
+    abdomen_gene = [ant1.abdomen_param, ant2.abdomen_param]
+    legs_len_gene = [ant1.legs_len_param, ant2.legs_len_param]
+    random_value = py5.random()
+    if random_value > 0.5:
+        ant3_head_gene = head_gene[0]
+    else:
+        ant3_head_gene = head_gene[1]
+    random_value = py5.random()
+    if random_value > 0.5:
+        ant3_body_gene = body_gene[0]
+    else:
+        ant3_body_gene = body_gene[1]
+    random_value = py5.random()
+    if random_value > 0.5:
+        ant3_abdomen_gene = abdomen_gene[0]
+    else:
+        ant3_abdomen_gene = abdomen_gene[1]
+    random_value = py5.random()
+    if random_value > 0.5:
+        ant3_legs_len_gene = legs_len_gene[0]
+    else:
+        ant3_legs_len_gene =legs_len_gene[1]
+    
+    ant3 = Ant(ant3_head_gene, ant3_body_gene, ant3_abdomen_gene, ant3_legs_len_gene)
+    ant3.show(150, 400)
+    """
+    for i in range(len(ants2)):
+        ants2[i].show(100 + 100 * i, 250)
+    for i in range(len(ants3)):
+        ants3[i].show(100 + 100 * i, 350)
+    for i in range(len(ants4)):
+        ants4[i].show(100 + 100 * i, 500)
+    """
+    #test_ant.show(250, 250)
+    py5.no_loop()
 py5.run_sketch()
